5f2d912352bcfc89d506f0f21338efcaf0686914
Manik1002-m <manik@sqelabs.com>
2026-02-11 18:37:40 +0530

    Access Request Type Status API is automated

--- src/services/assetmanagement.service.ts
+++ src/services/assetmanagement.service.ts
@@ -239,4 +239,23 @@ async getEmployeesDataV2 ( options?: RequestOptions)  {
     console.log("Get Employees Data V2 response:", response);
         return response
     }
+
+async getAccessRequestTypeStatus (
+    params: {
+    status?: string;
+    } = {},
+    options?: RequestOptions)  {
+
+    let response = await this.request.get(
+        ENDPOINTS.GET_ACCESS_REQUEST_TYPE_STATUS,
+        {
+            ...options,
+            params: {
+                status: params.status ?? ''
+            }
+        }
+    );
+    console.log("Get Access Request Type Status response:", response);
+        return response
+    }
 }
