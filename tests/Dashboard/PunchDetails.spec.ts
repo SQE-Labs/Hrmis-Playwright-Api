d71feec503e0875861d1e3062edaa99cc227babe
Manik1002-m <manik@sqelabs.com>
2026-02-18 12:04:09 +0530

    fixes and changes

--- tests/Dashboard/PunchDetails.spec.ts
+++ tests/Dashboard/PunchDetails.spec.ts
@@ -2,36 +2,40 @@ import {test, expect} from'../../fixtures/role.fixtures';
 
 test.describe('Punch Details', () => {
 
-    test("IM-03: Verify that the punch details are displayed correctly for a specific punch", async ({apiAsSuperAdmin}) => {
+    test("IM-03: Verify that the punch details are displayed correctly for a specific punch", async ({ apiAsSuperAdmin }) => {
         const res = await apiAsSuperAdmin.dashboard.getPunchDetails();
 
-        //Status code assertion
+        // Status code assertion
         expect(res.status).toBe(200);
 
-        //Response body assertions
-        expect(res.body).toHaveProperty('data');
-        expect(Array.isArray(res.body.data)).toBe(true);
-        expect(res.body.data.length).toBeGreaterThan(0);
+        const body = await res.body;
 
-        // Validate the structure of the first punch detail
-        res.body.data.forEach((punch: any) => {
-            expect(punch).toHaveProperty('date');
-            expect(typeof punch.date).toBe('string');
+        // Response body assertions
+        expect(body).toHaveProperty('data');
+        expect(Array.isArray(body.data)).toBe(true);
+        expect(body.data.length).toBeGreaterThan(0);
 
-            expect(punch).toHaveProperty('punch_in_time');
-            expect(typeof punch.punch_in_time).toBe('string');
+        // Access first punch record
+        const firstRecord = body.data[0];
 
-            expect(punch).toHaveProperty('punch_in_out');
-            expect(typeof punch.punch_in_out).toBe('string');
+        expect(firstRecord).toHaveProperty('work_entry');
+        expect(typeof firstRecord.work_entry).toBe('object');
 
-            expect(punch).toHaveProperty('total_hour');
-            expect(typeof punch.total_hour).toBe('string');
+        expect(firstRecord).toHaveProperty('login_minutes');
+        expect(typeof firstRecord.login_minutes).toBe('number');
 
-            expect(punch).toHaveProperty('is_positive_hour');
-            expect(typeof punch.is_positive_hour).toBe('boolean');
-        });
+        const punch = firstRecord.work_entry;
+
+        // Validate work_entry structure
+        expect(typeof punch.date).toBe('string');
+        expect(typeof punch.punch_in_time).toBe('string');
+        expect(typeof punch.punch_in_out).toBe('string');
+        expect(typeof punch.total_hour).toBe('string');
+        expect(typeof punch.is_positive_hour).toBe('boolean');
+        expect(typeof punch.is_compiled).toBe('boolean');
 
     });
+
 
     test("IM-03: Verify that the punch details are displayed correctly for a specific employee", async ({apiAsEmployee}) => {
         const res = await apiAsEmployee.dashboard.getPunchDetails();
